8eedee97113e0e3e7e259584f3afb277cb69b00a
ejsadiarin <ejs@ejsadiarin.com>
2025-03-17 02:36:08 +0800

    feat: new queries for user

--- sqlc/query.sql
+++ sqlc/query.sql
@@ -1,2 +1,8 @@
 -- name: GetUserNotifications :many
 SELECT * FROM notifications WHERE user_id = $1;
+
+-- name: AddUser :exec
+INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3);
+
+-- name: GetUser :one
+SELECT id, email, username, created_at FROM users WHERE id = $1;
